f1a83ad33ea48439245810b8517761f3662740bc
Flo Eberle <florian.eberle@competec.ch>
2022-06-09 17:01:14 +0200

    Demo: Maven Shade breaks actuator endpoint

---
+++ src/main/java/com/helloworld/demo/PingController.java
@@ -0,0 +1,18 @@
+package com.helloworld.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/test")
+public class PingController
+{
+    @GetMapping("/ping")
+    public ResponseEntity<String> pong()
+    {
+        return new ResponseEntity<>("pong", HttpStatus.OK);
+    }
+}
